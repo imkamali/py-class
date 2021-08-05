e76f67d641e03d5e679b2f113123b92a2ff1fe57
imkamali <87470578+imkamali@users.noreply.github.com>
2021-08-05 12:52:14 +0430

    Add files via upload

---
+++ s2_13.py
@@ -0,0 +1,6 @@
+name= input("what's your name? \n")
+age= int(input("how old are you? \n"))
+if (name== "Ali" and age>30) or (name!= "Ali" and age<20)
+    print("you can enter")
+else:
+    print("you can not enter")
