e9988ce8252c7a5fab12c2158f21a14187d24b00
imkamali <87470578+imkamali@users.noreply.github.com>
2021-08-26 11:25:59 +0430

    Add files via upload

---
+++ s4_10.py
@@ -0,0 +1,6 @@
+list = ['Hanieh', 'Maryam', 'Mohammadreza', 'Rasa', 'saeed','Roya']
+list1 =['Roya' , 'Raha', 'Hamid' , "Maryam"]
+list.extend(list1)
+
+myset=set(list)
+print(len(list)-(len(list)-len(myset)))
