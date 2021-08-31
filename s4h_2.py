e5de8a1260b6b06cdc0ce66345004947d8c4b400
imkamali <87470578+imkamali@users.noreply.github.com>
2021-08-31 21:11:28 +0430

    Add files via upload

---
+++ s4h_2.py
@@ -0,0 +1,11 @@
+#از کاربر نام،نام خانوادگی،وضیت تاهل را بگیرد دردیکشنری ذخیره و چاپ کند
+
+PersonalDetails = dict()
+
+PersonalDetails["Fname"]= input('Enter your Fname: \n')
+PersonalDetails["Lname"]= input('Enter your Lname: \n')
+PersonalDetails["Age"]= input('Enter your Age: \n')
+PersonalDetails["Marital status"]= input('Enter your Marital status: \n')
+
+print(PersonalDetails)
+
