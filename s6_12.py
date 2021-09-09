2456dbe907cc9c74f3e8db27548e267946a57695
imkamali <87470578+imkamali@users.noreply.github.com>
2021-09-09 12:43:50 +0430

    Update s6_12.py

--- s6_12.py
+++ s6_12.py
@@ -1,10 +1,9 @@
-result=1
+
 def factorial(n):
     if n==1:
         return
-    print((n+(n*(n-1))
-    factorial(n-1)
-    result=n+(n*(n-1)
-    print(result)
     
-factorial(5)
+    return n * factorial(n-1)
+    
+    
+print(factorial(5))
